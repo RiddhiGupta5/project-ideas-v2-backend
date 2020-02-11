8d45173283bbc1d913f6bdaa87569634a762fc13
RiddhiGupta5 <mail2riddhigupta@gmail.com>
2020-02-11 19:38:06 +0530

    Removed whitenoise

--- project_ideas/settings.py
+++ project_ideas/settings.py
@@ -201,8 +201,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
